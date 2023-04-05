038399739829510640df1c3551adfee1e22d8e2a
ApurvX210 <apurvsharma210@gmail.com>
2023-04-05 17:45:46 +0530

    Second Commit

--- src/App.js
+++ src/App.js
@@ -2,40 +2,81 @@ import { useState ,useEffect} from 'react';
 import './App.css';
 import Axios from 'axios';
 function App() {
-  const [foodName,setFoodName]=useState('');
-  const [days,setDays]=useState(0);
-  const [foodList,setfoodList]=useState([]);
+  const [uniqueId,setuniqueId]=useState('');
+  const [Name,setName]=useState('');
+  const [Age,setAge]=useState(0);
+  const [Data,setData]=useState([]);
+  const addToList=()=>{
+    if(uniqueId=='' || Name=='' || Age==null){
+      window.alert("Enter Valid Information");
+    }
+    Axios.post("http://localhost:3001/insert",{uniqueId:uniqueId,Name:Name,Age:Age}).catch((error) => {
+      window.alert("Data Invalid");
+    });;
+  }
+  const Delete=()=>{
+    if(uniqueId==''){
+      window.alert("Enter UniqueId Information");
+    }
+    Axios.post("http://localhost:3001/delete",{uniqueId:uniqueId});
+  }
+  const Update=()=>{
+    if(uniqueId==''){
+      window.alert("Enter UniqueId Information");
+    }
+    if(Name=='' && Age==null){
+      window.alert("Enter Valid Information");
+    }
+    Axios.put("http://localhost:3001/update",{uniqueId:uniqueId,Name:Name,Age:Age}).catch((error) => {
+      window.alert("Enter Valid Unique Id ");
+    });
+  }
+  // const Select=()=>{
+  //   Axios.post("http://localhost:3001/read",{uniqueId:uniqueId}).then((response)=>{
+  //     setData(response.data);
+  //   });
+  // }
   useEffect(()=>{
     Axios.get("http://localhost:3001/read").then((response)=>{
-      setfoodList(response.data);
-    })
+      setData(response.data);
+      console.log(Data);
+    });
   },[])
-  const addToList=()=>{
-    Axios.post("http://localhost:3001/insert",{foodName:foodName,days:days});
-  }
   return (
     <div className="App">
       <h1>CRUD APPLICATION</h1>
-      <label>Enter Food :</label>
+      <label>Enter UniqueId :</label>
       <input type="text" onChange={(event)=>{
-        setFoodName(event.target.value)
+        setuniqueId(event.target.value)
       }}></input>
-      <label>Enter Day :</label>
+      <label>Enter Name :</label>
+      <input type="text" onChange={(event)=>{
+        setName(event.target.value)
+      }}></input>
+      <label>Enter Age :</label>
       <input type="number" onChange={(event)=>{
-        setDays(event.target.value)
+        setAge(event.target.value)
       }}></input>
+      <div>
       <button onClick={addToList}>Add Data</button>
-      <h1>Food List</h1>
-      <div className="App">
+      <button onClick={Delete}>Delete Data</button>
+      <button onClick={Update}>Update Data</button>
+      {/* <button onClick={Select}>Select Data</button> */}
+      </div>
+      
+      <h1>Customer Detail</h1>
+      <div className="table-container">
       <tbody>
         <tr>
-          <th>Food Name</th>
-          <th>Price</th>
+          <th>UNIQUE ID</th>
+          <th>NAME</th>
+          <th>AGE</th>
         </tr>
-        {foodList.map((item, index) => (
+        {Data.map((item, index) => (
           <tr key={index}>
-            <td>{item.foodName}</td>
-            <td>{item.daysSinceIAte}</td>
+            <td>{item.uniqueId}</td>
+            <td>{item.Name}</td>
+            <td>{item.Age}</td>
           </tr>
         ))}
       </tbody>
